82abd9d66eb5b9e935d61428d3fa36d6da10ece4
Nicholas Licata <nicholas.licata730@gmail.com>
2023-03-11 21:33:33 -0500

    updated navbar functionality

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ hamburgerBtn.addEventListener('click', function() {
     navLinksContainer.classList.toggle('nav-links-container-active');
 })
 
+navLinks.forEach(link => link.addEventListener('click', function() {
+    if (navLinksContainer.classList.contains('nav-links-container-active')) {
+        navLinksContainer.classList.remove('nav-links-container-active');
+    } 
+}))
+
 window.addEventListener('resize', function() {
     if (window.innerWidth > 768) {
         navLinksContainer.classList.remove('nav-links-container-active');
